8da631aa3f99238765e54ee4a24f81f33184dc28
Aminu Bawa <eemanuthman21@gmail.com>
2023-05-12 10:38:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let btn=document.getElementById("btn");
+let changeword= document.querySelector("#change")
+
+
+btn.addEventListener("click",update);
+
+function update(){
+     changeword.innerHTML= "Javascript";
+     changeword.innerHTML
+}
